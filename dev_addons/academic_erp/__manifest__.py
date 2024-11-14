60d20e061d004a2915245a1d19e9190f4640eed2
Fabio Andre Arredondo Mendieta <fabioarredondo44@gmail.com>
2024-11-14 10:04:46 -0400

    correcion

--- dev_addons/academic_erp/__manifest__.py
+++ dev_addons/academic_erp/__manifest__.py
@@ -8,25 +8,24 @@
     """,
     'author': 'Dariana, Fabio y Lussiana',
     'depends': [
-        'base',      # Módulo base obligatorio
-        'calendar',  # Necesario para la vista de calendario
-        'mail',      # Para notificaciones y mensajes
-        'website'    # Opcional, para integrar con el portal web en caso de ser necesario
+        'base',      
+        'calendar',  
+        'mail',      
+        'website'    
     ],
     'data': [
         'security/academic_security.xml',
         'security/ir.model.access.csv',
-        'views/menu_views.xml',         # Agregamos el archivo de menús
+        'views/notice_calendar_view.xml',
+        'views/action_views.xml',
+        'views/menu_views.xml',
         'views/course_views.xml',
         'views/student_views.xml',
         'views/parent_views.xml',
         'views/teacher_views.xml',
         'views/subject_views.xml',
-        'views/notice_views.xml',  # Vista de aviso
-        'views/notice_calendar_view.xml',  # Vista de agenda
-        'views/notice_recipient_view.xml', #vista destinatario
-        'views/action_views.xml',       # Agregamos el archivo de acciones primero
-        
+        'views/notice_views.xml',
+        'views/notice_recipient_view.xml',
     ],
     'installable': True,
     'application': True,
